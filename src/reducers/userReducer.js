412383da73fcf801cebc96b00339f52733e92ed6
Christian <ChrisWcs@users.noreply.github.com>
2017-08-15 21:19:02 -0700

    Create userReducer to refactor out logic from mainReducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,9 @@
+import { USER_CHANGE, PASS_CHANGE } from '../actions/actions';
+
+const userReducer = (state, action) => {
+    switch(action.type){
+        default:
+    }
+};
+
+export default userReducer;
