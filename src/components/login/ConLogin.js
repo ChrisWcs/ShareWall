9ff339ecd0b4d9b58f91f8e465eeedb9b8a7a17b
Christian <ChrisWcs@users.noreply.github.com>
2017-08-15 19:39:47 -0700

    Begin creating mainReducer and initialState

--- src/components/login/ConLogin.js
+++ src/components/login/ConLogin.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux';
 import Login from './Login';
 
-const mapStateToProps = () => ({
-
-
+const mapStateToProps = (state) => ({
+    userVal: state.userVal,
+    passVal: state.passVal,
 });
 
-const mapDispatchToProps = () => ({
-
-
+const mapDispatchToProps = (dispatch) => ({
+    userValChange: () => {},
+    passValChange: () => {},
+    loginFunc: () => {},
 });
 
 const ConLogin = connect(mapStateToProps, mapDispatchToProps)(Login);
